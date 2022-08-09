845ed5964ab13b9e0da5d25dd08267654f9d4f23
YORQINJON-2305 <jakbaraliyevyorqinjon505@gmail.com>
2022-08-09 10:39:43 +0500

    Add contact page

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ var elOpenModal = document.querySelector(".paw-btn--open");
 var elModal = document.querySelector(".modal");
 var elCloseModal = document.querySelector(".modal__close-btn");
 
+var elReadMore = document.querySelectorAll(".paw-btn--status")
+var elReadMoreItem = document.querySelectorAll(".status__item")
+
 elOpenModal.addEventListener("click", function(){
     elModal.classList.add("open-modal");
 })
@@ -9,3 +12,14 @@ elOpenModal.addEventListener("click", function(){
 elCloseModal.addEventListener("click", function(){
     elModal.classList.remove("open-modal");
 })
+
+elReadMore.forEach(function (btn){
+    btn.addEventListener("click", function(evt){
+        evt.preventDefault();
+
+    elReadMoreItem.forEach(function(item){
+        item.classList.remove("status__item--active")
+    })
+        
+  })
+});
